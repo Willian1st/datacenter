73fe37804796e4c9c208bdfa951760b5d7a2bbbc
gaowei <123>
2019-06-12 16:43:38 +0800

    自动学习

--- Ws tool/background.js
+++ Ws tool/background.js
@@ -1,134 +1,183 @@
 ﻿var Notification = window.Notification || window.mozNotification || window.webkitNotification;
 var notify = new window.Notification('欢迎使用！');
 if (window.Notification.permission == 'granted') {
-	notify.onclick = function () {
-		// 如果通知消息被点击,通知窗口将被激活
-		window.focus();
-	},
-	notify.onerror = function () {
-		console.log("桌面消息出错！");
-	};
-	notify.onshow = function () {
-		setTimeout(function () {
-			notify.close();
-		}, 2000)
-	};
-	notify.onclose = function () {
-		console.log("桌面消息关闭！");
-	};
+    notify.onclick = function () {
+        // 如果通知消息被点击,通知窗口将被激活
+        window.focus();
+    },
+        notify.onerror = function () {
+            console.log("桌面消息出错！");
+        };
+    notify.onshow = function () {
+        setTimeout(function () {
+            notify.close();
+        }, 2000)
+    };
+    notify.onclose = function () {
+        console.log("桌面消息关闭！");
+    };
 } else {
-	console.log("桌面消息未打开！");
+    console.log("桌面消息未打开！");
 }
 chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
-	if (request.cmd == "history") {
-		chrome.history.search({
-			text: '',
-			maxResults: 10
-		}, function (result) {
-			var url = [];
-			$.each(result, function (i, v) {
-				url.push(v.url);
-			});
-			sendResponse({
-				result: url
-			});
-			var content = "";
-			$.each(url, function (i, v) {
-				content += (v + "<br>");
-			});
-			var notice = new window.Notification("Tips", {
-					icon: "icon16.png",
-					body: "页面已打开：" + content
-				});
-			notice.onshow = function () {
-				setTimeout(function () {
-					notice.close();
-				}, 2000);
-			};
-		})
-	} else if (request.cmd == "tab") {
-		chrome.tabs.getAllInWindow(function (result) {
-			console.log(result)
-		})
-		chrome.tabs.create({
-			url: "chrome://extensions/"
-		}, function (result) {
-			var notice = new window.Notification("Tips", {
-					icon: "icon16.png",
-					body: "页面已打开：" + result.url
-				});
-			notice.onshow = function () {
-				setTimeout(function () {
-					notice.close();
-				}, 2000);
-			};
-		});
-		sendResponse({
-			result: request.cmd
-		});
-	} else if (request.cmd == "suning") {
-		chrome.tabs.create({
-			url: "http://sign.suning.com/sign/welcome.do"
-		}, function (result) {
-			var notice = new window.Notification("Tips", {
-					icon: "icon16.png",
-					body: "页面已打开：" + result.url
-				});
-			$(".starttip").click();
-			notice.onshow = function () {
-				setTimeout(function () {
-					notice.close();
-				}, 2000);
-			};
-		});
-		sendResponse({
-			result: request.cmd
-		});
-	} else if (request.cmd == "guomei") {
-		chrome.tabs.create({
-			url: "http://club.gome.com.cn/member/memberClub"
-		}, function (result) {});
-		sendResponse({
-			result: request.cmd
-		});
-	} else {
-		sendResponse({
-			result: "hi"
-		});
-	}
+    if (request.cmd == "history") {
+        chrome.history.search({
+            text: '',
+            maxResults: 10
+        }, function (result) {
+            var url = [];
+            $.each(result, function (i, v) {
+                url.push(v.url);
+            });
+            sendResponse({
+                result: url
+            });
+            var content = "";
+            $.each(url, function (i, v) {
+                content += (v + "<br>");
+            });
+            var notice = new window.Notification("Tips", {
+                icon: "icon16.png",
+                body: "页面已打开：" + content
+            });
+            notice.onshow = function () {
+                setTimeout(function () {
+                    notice.close();
+                }, 2000);
+            };
+        })
+    } else if (request.cmd == "tab") {
+        chrome.tabs.getAllInWindow(function (result) {
+            console.log(result)
+        });
+        chrome.tabs.create({
+            url: "chrome://extensions/"
+        }, function (result) {
+            var notice = new window.Notification("Tips", {
+                icon: "icon16.png",
+                body: "页面已打开：" + result.url
+            });
+            notice.onshow = function () {
+                setTimeout(function () {
+                    notice.close();
+                }, 2000);
+            };
+        });
+        sendResponse({
+            result: request.cmd
+        });
+    } else if (request.cmd == "suning") {
+        chrome.tabs.create({
+            url: "http://sign.suning.com/sign/welcome.do"
+        }, function (result) {
+            var notice = new window.Notification("Tips", {
+                icon: "icon16.png",
+                body: "页面已打开：" + result.url
+            });
+            $(".starttip").click();
+            notice.onshow = function () {
+                setTimeout(function () {
+                    notice.close();
+                }, 2000);
+            };
+        });
+        sendResponse({
+            result: request.cmd
+        });
+    } else if (request.cmd == "guomei") {
+        chrome.tabs.create({
+            url: "http://club.gome.com.cn/member/memberClub"
+        }, function (result) {
+        });
+        sendResponse({
+            result: request.cmd
+        });
+    } else if (request.cmd == "xuexi") {
+        chrome.tabs.create({
+            url: "https://www.xuexi.cn/"
+        }, function (result) {
+            var notice = new window.Notification("Tips", {
+                icon: "icon16.png",
+                body: "准备开始学习..."
+            });
+            notice.onshow = function () {
+                setTimeout(function () {
+                    notice.close();
+                }, 5000);
+            };
+        });
+        sendResponse({
+            result: request.cmd
+        });
+    } else if (request.cmd == "xuexi_news") {
+        var notice = new window.Notification("Tips", {
+            icon: "icon16.png",
+            body: "正在学习文字新闻..."
+        });
+        notice.onshow = function () {
+            setTimeout(function () {
+                notice.close();
+            }, 5000);
+        };
+        sendResponse({
+            result: request.cmd
+        });
+    } else if (request.cmd == "xuexi_video_news") {
+        chrome.tabs.create({
+            url: "https://www.xuexi.cn/4426aa87b0b64ac671c96379a3a8bd26/db086044562a57b441c24f2af1c8e101.html#11c4o0tv7nb-5"
+        }, function (result) {
+            var notice = new window.Notification("Tips", {
+                icon: "icon16.png",
+                body: "准备开始学习视频..."
+            });
+            notice.onshow = function () {
+                setTimeout(function () {
+                    notice.close();
+                }, 5000);
+            };
+        });
+        sendResponse({
+            result: request.cmd
+        });
+    } else {
+        sendResponse({
+            result: "hi"
+        });
+    }
 });
 
 chrome.contextMenus.create({
-	type: "normal",
-	contexts: ["image"],
-	title: "预览",
-	onclick: function (info, tab) {
-		console.log(info);
-		if (info.mediaType == 'image') {
-			chrome.tabs.query({
-				title: "配置"
-			}, function (tabs) {
-				console.table(tabs)
-				if (tabs && tabs[0]) {
-					chrome.extension.sendRequest({
-						cmd: 'internetUrl',
-						internetUrl: info.srcUrl
-					}, function (response) {
-						console.log(response);
-					});
-				} else {
-					chrome.tabs.create({
-						url: "chrome-extension://" + chrome.runtime.id + "/options.html#/map?internetUrl="
-						 + info.srcUrl
-					}, function (result) {
-						console.log("预览:" + result);
-					});
-				}
+    type: "normal",
+    contexts: ["image"],
+    title: "预览",
+    onclick: function (info, tab) {
+        console.log(info);
+        if (info.mediaType == 'image') {
+            chrome.tabs.query({
+                title: "配置"
+            }, function (tabs) {
+                console.table(tabs)
+                if (tabs && tabs[0]) {
+                    chrome.extension.sendRequest({
+                        cmd: 'internetUrl',
+                        internetUrl: info.srcUrl
+                    }, function (response) {
+                        console.log(response);
+                    });
+                } else {
+                    chrome.tabs.create({
+                        url: "chrome-extension://" + chrome.runtime.id + "/options.html#/map?internetUrl="
+                            + info.srcUrl
+                    }, function (result) {
+                        console.log("预览:" + result);
+                    });
+                }
 
-			})
+            })
 
-		} else {}
-	}
+        } else {
+        }
+    }
 }, function (result) {
-	console.log("右键菜单初始化完成");
+    console.log("右键菜单初始化完成");
 });
--- Ws tool/content_script.js
+++ Ws tool/content_script.js
@@ -1,338 +1,332 @@
-﻿$(function() {
-	window.alert = layer.msg;
-	hotkeys('shift+a', function(event, handler) {
-		submit('admin02');
-	});
-	hotkeys('shift+y', function(event) {
-		submit('sys');
-	});
-	hotkeys('shift+s', function(event) {
-		submit('sysAdmin');
-	});
-	hotkeys('fn+s', function(event) {
-		submit('sysAdmin', "zq123456");
-	});
-	hotkeys('shift+m', function(event) {
-		submit('mlmkAdmin');
-	});
-	hotkeys('shift+w', function(event) {
-		submit('wfhg');
-	});
-	hotkeys('shift+b', function(event) {
-		submit('bjyyt');
-	});
-	hotkeys('shift+l', function(event) {
-		submit('lpsAdmin');
-	});
-	hotkeys('shift+z', function(event) {
-		submit('lps_zsq_Admin');
-	});
-	hotkeys('shift+g', function(event) {
-		submit('gzrjaqkj');
-	});
-	hotkeys('fn+esc', function() {
-		var button = document.getElementById("logoutHref");
-		button.click();
-	});
-	hotkeys('alt+1', function() {
-		location.href = "http://localhost:7070/cas/login";
-	});
-	hotkeys('alt+2', function() {
-		location.href = "http://www.gzaqy.com/cas/login";
-	});
+﻿$(function () {
+    window.alert = layer.msg;
+    hotkeys('shift+a', function (event, handler) {
+        submit('admin02');
+    });
+    hotkeys('shift+y', function (event) {
+        submit('sys');
+    });
+    hotkeys('shift+s', function (event) {
+        submit('sysAdmin');
+    });
+    hotkeys('fn+s', function (event) {
+        submit('sysAdmin', "zq123456");
+    });
+    hotkeys('shift+m', function (event) {
+        submit('mlmkAdmin');
+    });
+    hotkeys('shift+w', function (event) {
+        submit('wfhg');
+    });
+    hotkeys('shift+b', function (event) {
+        submit('bjyyt');
+    });
+    hotkeys('shift+l', function (event) {
+        submit('lpsAdmin');
+    });
+    hotkeys('shift+z', function (event) {
+        submit('lps_zsq_Admin');
+    });
+    hotkeys('shift+g', function (event) {
+        submit('gzrjaqkj');
+    });
+    hotkeys('fn+esc', function () {
+        var button = document.getElementById("logoutHref");
+        button.click();
+    });
+    hotkeys('alt+1', function () {
+        location.href = "http://localhost:7070/cas/login";
+    });
+    hotkeys('alt+2', function () {
+        location.href = "http://www.gzaqy.com/cas/login";
+    });
 
-	hotkeys('alt+h', function() {
-		chrome.extension.sendRequest({
-			cmd : "history"
-		}, function(response) {
-			console.log(response.result);
-		});
-	});
-	hotkeys('shift+tab', function() {
-		chrome.extension.sendRequest({
-			cmd : "tab"
-		}, function(response) {
-			console.log(response.result);
-		});
-	});
-	hotkeys('shift+ctrl+alt+s', function() {
-		// 打卡-suning
-		chrome.extension.sendRequest({
-			cmd : "suning"
-		}, function(response) {
-			return;
-			console.log(response.result);
-		});
-	});
+    hotkeys('alt+h', function () {
+        chrome.extension.sendRequest({
+            cmd: "history"
+        }, function (response) {
+            console.log(response.result);
+        });
+    });
+    hotkeys('shift+tab', function () {
+        chrome.extension.sendRequest({
+            cmd: "tab"
+        }, function (response) {
+            console.log(response.result);
+        });
+    });
+    hotkeys('shift+ctrl+alt+s', function () {
+        // 打卡-suning
+        chrome.extension.sendRequest({
+            cmd: "suning"
+        }, function (response) {
+            return;
+            console.log(response.result);
+        });
+    });
 
-	hotkeys('shift+q', function() {
-		// 打卡-guomei
-		chrome.extension.sendRequest({
-			cmd : "guomei"
-		}, function(response) {
-			debugger
-			console.log(response.result);
-			return;
-			setTimeout(function() {
-				if ($("#tabRight")) {
-					$("#tabRight").click();
-				}
-			}, 3000);
-		});
-	});
+    hotkeys('shift+q', function () {
+        // 打卡-guomei
+        chrome.extension.sendRequest({
+            cmd: "guomei"
+        }, function (response) {
+            debugger
+            console.log(response.result);
+            return;
+            setTimeout(function () {
+                if ($("#tabRight")) {
+                    $("#tabRight").click();
+                }
+            }, 3000);
+        });
+    });
+
+    hotkeys('shift+x', function () {
+        chrome.extension.sendRequest({
+            cmd: "xuexi"
+        });
+    });
+
+    hotkeys('ctrl+shift+x', function () {
+        xuexi();
+        chrome.extension.sendRequest({
+            cmd: "xuexi_video_news"
+        });
+    });
 
 });
-submit = function(userName, passWord) {
-	var users = [ {
-		name : 'sys',
-		code : 'longruan++'
-	}, {
-		name : 'sysAdmin',
-		code : 'aqy+123'
-	}, {
-		name : 'admin02',
-		code : 'admin02'
-	}, {
-		name : 'mlmkAdmin',
-		code : '123456'
-	}, {
-		name : 'wfhg',
-		code : '123456'
-	}, {
-		name : 'gzrjaqkj',
-		code : '123456'
-	}, {
-		name : 'lpsAdmin',
-		code : '123456'
-	}, {
-		name : 'qxnzxrajj',
-		code : '123456'
-	}, {
-		name : 'lps_zsq_Admin',
-		code : '123456'
-	}, {
-		name : 'zyajj',
-		code : '123456'
-	}, {
-		name : 'bjsajj',
-		code : '123456'
-	}, {
-		name : 'sysAdmin_zq',
-		code : 'zq123456'
-	}, {
-		name : 'bjyyt',
-		code : '888888'
-	}, {
-		name : 'bzqmtzajbckm',
-		code : '123456'	
-	}, {
-		name : 'xlczajz',
-		code : '123456'
-	}	];
-	setTimeout(
-			function() {
-				var username = $("[name='username']");
-				if (username.length == 0) {
-					username = $(document.getElementsByClassName('username')[0]);
-				}
-				if (username.length == 0) {
-					username = document.getElementsByTagName('iframe')[0].contentWindow.document
-							.getElementById('username');
-				}
-				var password = $("[name='password']");
-				if (password.length == 0) {
-					password = $(document.getElementsByClassName('password')[0]);
-				}
-				if (password.length == 0) {
-					password = document.getElementsByTagName('iframe')[0].contentWindow.document
-							.getElementById('password');
-				}
-				if (!(username instanceof jQuery)) {
-					username = $(username);
-				}
-				if (!(password instanceof jQuery)) {
-					password = $(password);
-				}
-				username.focus().val('');
-				password.focus().val('');
-				var form = document.getElementById('fm1')
-						|| document.getElementById('fm11') || document.getElementById('fm2');
-				if (!form) {
-					form = document.getElementsByClassName('login-btn')[0];
-				}
-				if (!form && document.getElementsByTagName('iframe')[0]) {
-					form = document.getElementsByTagName('iframe')[0].contentWindow.document
-							.getElementById('fm1');
-				}
-				username.val(userName.replace("_zq", ""));
-				password.val(passWord || (function(userName) {
-					var code = '';
-					$.each(users, function(i, v) {
-						if (v.name == userName) {
-							code = v.code;
-						}
-					});
-					return code;
-				})(userName));
-				if (form) {
-					if (form.submit) {
-						form.submit();
-					} else {
-						form.click();
-					}
-					var gointo = document.getElementsByTagName('iframe')[0].contentWindow.document
-							.getElementById('go-success');
-					setInterval(
-							function() {
-								if (gointo) {
-									gointo.click();
-								} else {
-									gointo = document
-											.getElementsByTagName('iframe')[0].contentWindow.document
-											.getElementById('go-success');
-								}
-							}, 1000);
-				}
-			}, 1000);
+submit = function (userName, passWord) {
+    var users = [{
+        name: 'sys',
+        code: 'longruan++'
+    }, {
+        name: 'sysAdmin',
+        code: 'aqy+123'
+    }, {
+        name: 'admin02',
+        code: 'admin02'
+    }, {
+        name: 'mlmkAdmin',
+        code: '123456'
+    }, {
+        name: 'wfhg',
+        code: '123456'
+    }, {
+        name: 'gzrjaqkj',
+        code: '123456'
+    }, {
+        name: 'lpsAdmin',
+        code: '123456'
+    }, {
+        name: 'qxnzxrajj',
+        code: '123456'
+    }, {
+        name: 'lps_zsq_Admin',
+        code: '123456'
+    }, {
+        name: 'zyajj',
+        code: '123456'
+    }, {
+        name: 'bjsajj',
+        code: '123456'
+    }, {
+        name: 'sysAdmin_zq',
+        code: 'zq123456'
+    }, {
+        name: 'bjyyt',
+        code: '888888'
+    }, {
+        name: 'bzqmtzajbckm',
+        code: '123456'
+    }, {
+        name: 'xlczajz',
+        code: '123456'
+    }];
+    setTimeout(
+        function () {
+            var username = $("[name='username']");
+            if (username.length == 0) {
+                username = $(document.getElementsByClassName('username')[0]);
+            }
+            if (username.length == 0) {
+                username = document.getElementsByTagName('iframe')[0].contentWindow.document
+                    .getElementById('username');
+            }
+            var password = $("[name='password']");
+            if (password.length == 0) {
+                password = $(document.getElementsByClassName('password')[0]);
+            }
+            if (password.length == 0) {
+                password = document.getElementsByTagName('iframe')[0].contentWindow.document
+                    .getElementById('password');
+            }
+            if (!(username instanceof jQuery)) {
+                username = $(username);
+            }
+            if (!(password instanceof jQuery)) {
+                password = $(password);
+            }
+            username.focus().val('');
+            password.focus().val('');
+            var form = document.getElementById('fm1')
+                || document.getElementById('fm11') || document.getElementById('fm2');
+            if (!form) {
+                form = document.getElementsByClassName('login-btn')[0];
+            }
+            if (!form && document.getElementsByTagName('iframe')[0]) {
+                form = document.getElementsByTagName('iframe')[0].contentWindow.document
+                    .getElementById('fm1');
+            }
+            username.val(userName.replace("_zq", ""));
+            password.val(passWord || (function (userName) {
+                var code = '';
+                $.each(users, function (i, v) {
+                    if (v.name == userName) {
+                        code = v.code;
+                    }
+                });
+                return code;
+            })(userName));
+            if (form) {
+                if (form.submit) {
+                    form.submit();
+                } else {
+                    form.click();
+                }
+                var gointo = document.getElementsByTagName('iframe')[0].contentWindow.document
+                    .getElementById('go-success');
+                setInterval(
+                    function () {
+                        if (gointo) {
+                            gointo.click();
+                        } else {
+                            gointo = document
+                                .getElementsByTagName('iframe')[0].contentWindow.document
+                                .getElementById('go-success');
+                        }
+                    }, 1000);
+            }
+        }, 1000);
 };
-sendMessage = function(keys) {
-	// 给background和popup发送数据
-	chrome.extension.sendRequest({
-		cmd : JSON.stringify(keys)
-	}, function(response) {
-		return;
-		console.log(response.result);
-	});
-}
+sendMessage = function (keys) {
+    // 给background和popup发送数据
+    chrome.extension.sendRequest({
+        cmd: JSON.stringify(keys)
+    }, function (response) {
+        return;
+        console.log(response.result);
+    });
+};
 // 接收popup发来的消息
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-	if (request.message == "logout") {
-		var button = document.getElementById("logoutHref");
-		if (button) {
-			button.click();
-			layer.msg("退出");
-		}
-	} else if (request.message == "clean") {
-		var url = location.href;
-		if (url) {
-			var reg = new RegExp("(^|;)jsessionid=([^#]*)");
-			var r = url.substr(1).match(reg);
-			if (r != null) {
-				var result = ";jsessionid=" + unescape(r[2]);
-				history.pushState(null, null, url.replace(result, ""));
-			}
-		}
-	} else if (request.message) {
-		submit(request.message);
-	}
+chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.message == "logout") {
+        var button = document.getElementById("logoutHref");
+        if (button) {
+            button.click();
+            layer.msg("退出");
+        }
+    } else if (request.message == "clean") {
+        var url = location.href;
+        if (url) {
+            var reg = new RegExp("(^|;)jsessionid=([^#]*)");
+            var r = url.substr(1).match(reg);
+            if (r != null) {
+                var result = ";jsessionid=" + unescape(r[2]);
+                history.pushState(null, null, url.replace(result, ""));
+            }
+        }
+    } else if (request.message) {
+        submit(request.message);
+    }
 });
+
 // 获取url中的参数
 function getUrlParam(name) {
-	var reg = new RegExp("(^|;)" + name + "=([^&]*)(&|$)");
-	var r = window.location.search.substr(1).match(reg);
-	if (r != null)
-		return unescape(r[2]);
-	return null;
+    var reg = new RegExp("(^|;)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null)
+        return unescape(r[2]);
+    return null;
 }
+
 if (location.href.indexOf("localhost") == -1) {
-	// $("body").append("<style>h1,h2,h3,h4,h5,p,body,div,span,label,a,i,form,table,th,tr,td,ul,li,figure,pre,button,header,code,textarea{background-color:#2b2b2b!important;color:#bbbbbb;}</style>");
+    // $("body").append("<style>h1,h2,h3,h4,h5,p,body,div,span,label,a,i,form,table,th,tr,td,ul,li,figure,pre,button,header,code,textarea{background-color:#2b2b2b!important;color:#bbbbbb;}</style>");
 }
-$(function() {
-	return;
-	// guomei start
-	if ($(".tabRight")) {
-		setTimeout(function() {
-			// 切换登录框
-			$(".tabRight").click();
-			setTimeout(function() {
-				// 点击登录
-				// $("#loginName").val(15195772530);
-				// $("#loginPassword").val("");
-				$(".actions .btnnuw").click();
-			}, 1000);
-		}, 1000);
+var xuexi = function () {
+    if (location.href.indexOf("www.xuexi.cn") != -1) {
+        //获取新闻列表
+        var section = $("[data-data-id]").siblings("div section");
+        var listNews = [];
+        if (section.length) {
+            section.each(function () {
+                var title = $(this).find(".text-link-item-title .text-wrap:first");
+                if (title && title.length && $.trim(title.text()) != '') {
+                    listNews.push(title);
+                }
+            });
+        } else {
+            listNews = $(".text-wrap");
+        }
+        //打开前六个文字新闻
+        $.each(listNews, function (i, v) {
+            v = $(v);
+            if (i < 10) {
+                setTimeout(function () {
+                    v.click();
+                }, i * 125000);
+            }
+        });
+    }
+};
 
-	} else {
-		console.log("登录框不存在");
-	}
-	if ($(".sign-btn,.sign-btn-text")) {
-		setTimeout(
-				function() {
-					// 点击签到
-					$(".sign-btn,.sign-btn-text").click();
-					if ($(".sign-btn,.sign-btn-text")) {
-						setTimeout(
-								function() {
-									// 进入抽奖界面
-									if ($(".mdcj") && $(".mdcj")[0]) {
-										location.href = "//prom.gome.com.cn/html/prodhtml/topics/201608/11/8952408833.html";
-
-									}
-								}, 1000);
-					} else {
-						console.log("抽奖链接不存在");
-					}
-				}, 1000);
-
-	} else {
-		console.log("签到不存在");
-	}
-
-	if ($("#m") || $("#m")[0]) {
-		setTimeout(
-				function() {
-					// 点击抽奖
-					if ($(".luck .bt")) {
-						setTimeout(function() {
-							$(".luck .bt").click();
-						}, 1000);
-						setTimeout(
-								function() {
-									// 点击关闭
-									if ($("#showInfo .j-gl-c")
-											&& $("#showInfo .j-gl-c")[0]) {
-										$("#showInfo .j-gl-c")[0].click();
-										setTimeout(
-												function() {
-													// 点击抽奖
-													if ($(".luck .bt")) {
-														$(".luck .bt").click();
-														setTimeout(
-																function() {
-																	// 点击关闭
-																	if ($("#showInfo .j-gl-c")) {
-																		$("#showInfo .j-gl-c")[0]
-																				.click();
-																		setTimeout(
-																				function() {
-																					// 退出
-																					if ($(".gome-link-logout")[0]) {
-																						location.href = "//login.gome.com.cn/login?type=logout";
-																					}
-																				},
-																				1000);
-																	}
-																}, 8000);
-													}
-												}, 1000);
-									}
-								}, 8000);
-					} else {
-						console.log("抽奖按钮不存在");
-					}
-				}, 1000);
-	} else {
-		console.log("界面不存在");
-	}
-	// guomei end
-	if ($("#popBox-login-button") && $("#popBox-login-button")[0]) {
-		$("#popBox-login-button")[0].click();
-	} else {
-		console.log("10086登录按钮不存在");
-	}
-	if ($("#doSign") && $("#doSign")[0]) {
-		$("#doSign")[0].click();
-	} else {
-		console.log("e币按钮不存在");
-	}
+$(function () {
+    if (location.href.indexOf("lgpage/detail/index.html?id=") != -1) {
+        chrome.extension.sendRequest({
+            cmd: "xuexi_news"
+        });
+        var documentHeight = $(document).height();
+        var windowHeight = window.innerHeight;
+        var backHeight = -windowHeight;
+        var scrollHeight = documentHeight - windowHeight;
+        var flag = setInterval(function () {
+            scrollHeight = scrollHeight - 50;
+            if (scrollHeight < 0) {
+                clearInterval(flag);
+                window.scrollBy(0, backHeight);
+            } else {
+                window.scrollBy(0, 50);
+            }
+        }, 1000);
+        //新闻2分钟后关闭
+        setTimeout(function () {
+            window.close();
+        }, 125000);
+    } else if (location.href.indexOf("4426aa87b0b64ac671c96379a3a8bd26/db086044562a57b441c24f2af1c8e101.html#11c4o0tv7nb-5") != -1) {
+        //新闻联播列表界面
+        setTimeout(function () {
+            $(".tab-wrapper div:contains('新闻联播')").click();
+            setTimeout(function () {
+                var videoNews = $(".grid-cell .textWrapper");
+                //打开前四个视频新闻
+                $.each(videoNews, function (i, v) {
+                    v = $(v);
+                    if (i < 8) {
+                        setTimeout(function () {
+                            v.click();
+                        }, i * 187000);
+                    }
+                });
+            }, 5000);
+        }, 5000);
+    } else if (location.href.indexOf("7f9f27c65e84e71e1b7189b7132b4710.html") != -1) {
+        setTimeout(function () {
+            var video = document.getElementsByTagName("video")[0];
+            video.muted = true;
+            video.play();
+        }, 5000);
+        //新闻联播视频3分钟后关闭
+        setTimeout(function () {
+            window.close();
+        }, 185000);
+    }
 });
